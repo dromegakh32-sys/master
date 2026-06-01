8fa52ec78b2d1c8caaa679df2bee9afbc5685912
HanOon_32 <dr.omegakh32@gmail.com>
2026-06-01 10:10:29 +0000

    Mon, Jun 1, 2026, 10:10 AM +00:00

--- hani.py/Hanoon.py/Python training with Harvard University1/hello.py
+++ hani.py/Hanoon.py/Python training with Harvard University1/hello.py
@@ -235,11 +235,19 @@ elif x > y:
 elif x == y:
     print("x is equal to y")
 
+# نفس الكود باستخدام else
+# The same code using else
 
 
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
-
-
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
 
 
 
